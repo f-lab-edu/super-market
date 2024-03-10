5410818e83d91bb208b7b74d5cd920f6ed88a165
hyeoKing <cksgurwkd12@naver.com>
2024-03-11 02:00:03 +0900

    api-gateway 서버 기본 설정 및 프로젝트 전체에 대한 git ignore 설정
    - api-gateway 서버를 위한 Spring Boot Application 생성
    - build.gradle에 api-gateway 서버에 필요한 의존성 정의
    - 프로젝트 전체에 적용될 git ignore 정의

---
+++ supermarket-api-gateway/src/main/java/com/harmony/supermarketapigateway/SupermarketApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.harmony.supermarketapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketApiGatewayApplication.class, args);
+	}
+
+}
