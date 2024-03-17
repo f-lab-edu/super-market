9df60c23f61341e4beea03615b5a26441af45c1e
hyeoKing <cksgurwkd12@naver.com>
2024-03-17 23:51:05 +0900

    PRODUCT 도메인 개발을 위한 프로젝트 세팅
    [의존성]
    - spring-boot-starter-data-jpa: 프로젝트 초기에 쿼리 변경들 작업을 빠르고 간편하게 하기 위해 적용
    - spring-boot-starter-validation: 검증 작업을 보기 편하고 편리하게 관리하기 위해 적용
    - spring-boot-starter-web: 스프링 웹 개발을 위해 적용
    - lombok: AllArgsConstruct등 불필요한 코드를 줄여주기 위해 lombok 사용
    - h2: 테스트용 인메모리 DB
    
    [gradle 관련 파일 추가 이유]
    - 최대한 코드 관련 내용만 PR 하는 방향을 고려했으나, 여러 PC를 옮겨가며 사용해야 하는 경우가 종종 있어 부득이하게 빌드 파일 추가

---
+++ supermarket-api-product/src/main/java/com/harmony/supermarketapiproduct/SupermarketApiProductApplication.java
@@ -0,0 +1,13 @@
+package com.harmony.supermarketapiproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketApiProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermarketApiProductApplication.class, args);
+    }
+
+}
---
+++ supermarket-api-product/src/test/java/com/harmony/supermarketapiproduct/SupermarketApiProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harmony.supermarketapiproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketApiProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
