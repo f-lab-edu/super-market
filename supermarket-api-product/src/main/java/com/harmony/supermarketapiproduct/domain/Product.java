e9452e511303bc366de25ab7b23723ad09b6ef4a
hyeoKing <cksgurwkd12@naver.com>
2024-03-25 01:05:43 +0900

    PRODUCT 도메인 개발
    [작업 내용]
    - 상품 재고 감소를 위한 decreaseStock() 메서드 작성
    - 상품 재고 값이 예상한 값과 같은지 비교 하기 위한 verifyStockAmount() 메서드 개발
    - ProductDto 로 변환을 위한 toProductDto() 메서드 개발
    
    [테스트 케이스]
    - 성공 케이스: 재고 감소 해피 케이스
    - 실패 케이스(1): 재고 감소의 결과가 재고를 0 미만 으로 만들 경우 예외 발생 케이스
    - 실패 케이스(2): 재고 감소 요청 으로 넘어온 재고 감소 수량이 음수인 경우 예외 발생 케이스

---
+++ supermarket-api-product/src/main/java/com/harmony/supermarketapiproduct/domain/Product.java
@@ -0,0 +1,47 @@
+package com.harmony.supermarketapiproduct.domain;
+
+
+import com.harmony.supermarketapiproduct.application.dto.ProductDto;
+import com.harmony.supermarketapiproduct.common.exception.ProductInsufficientStockException;
+import com.harmony.supermarketapiproduct.common.exception.ProductQuantityNegativeException;
+import com.harmony.supermarketapiproduct.common.exception.ProductStockMismatchException;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    private String name;
+
+    private Long price;
+
+    private Integer stockQuantity;
+
+    @Version
+    private Long version;
+
+    public Product(final String name, final Long price, final Integer stockQuantity){
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public ProductDto toProductDto(){ return new ProductDto(this.productId, this.name, this.price, this.stockQuantity); }
+
+    public void decreaseStock(final Integer quantity){
+        if (quantity < 0) { throw new ProductQuantityNegativeException(); }
+        if (this.stockQuantity < quantity){ throw new ProductInsufficientStockException(); }
+
+        this.stockQuantity -= quantity;
+    }
+
+    public void verifyStockAmount(final Integer expectedStock){
+        if (!this.stockQuantity.equals(expectedStock)) { throw new ProductStockMismatchException(); }
+    }
+}
