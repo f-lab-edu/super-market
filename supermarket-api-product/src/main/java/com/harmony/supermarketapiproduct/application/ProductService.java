828c0b0001f2ef41f334c88269836e4e20c29a4d
hyeoKing <cksgurwkd12@naver.com>
2024-03-30 01:48:04 +0900

    [#25] ProductService 위치 변경
    [작업 내용]
    - ProductService.java의 경로를 supermarket-api-product/src/main/java/com/harmony/supermarketapiproduct/application으로 변경
    
    [테스트 내용]
    - ProductService.java의 경로 변경 이후 스프링 부트 애플리케이션 실행 및 정상 동작 확인

--- supermarket-api-product/src/main/java/com/harmony/supermarketapiproduct/application/ProductService.java
+++ supermarket-api-product/src/main/java/com/harmony/supermarketapiproduct/application/ProductService.java
@@ -1,8 +1,56 @@
 package com.harmony.supermarketapiproduct.application;
 
+import com.harmony.supermarketapiproduct.application.dto.ProductDto;
+import com.harmony.supermarketapiproduct.application.dto.StockDecreaseDto;
+import com.harmony.supermarketapiproduct.common.exception.ProductNotFoundException;
+import com.harmony.supermarketapiproduct.common.exception.ProductStockUpdateException;
+import com.harmony.supermarketapiproduct.domain.Product;
+import com.harmony.supermarketapiproduct.domain.ProductRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Component;
 
-@Component
-public class ProductService {
+import java.util.ArrayList;
+import java.util.List;
 
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+    private final ProductRepository productRepository;
+    private final long MAX_RETRY_ATTEMPTS = 3;
+
+    @Transactional
+    public List<ProductDto> decreaseStock(final List<StockDecreaseDto> stockDecreaseDtos) {
+        List<ProductDto> productDtoList = new ArrayList<>();
+
+        for (StockDecreaseDto stockDecreaseDto: stockDecreaseDtos){
+            boolean success = false;
+            int attempts = 0;
+
+            while(!success && attempts < MAX_RETRY_ATTEMPTS) {
+                try {
+                    Product product = productRepository.findById(stockDecreaseDto.getProductId())
+                            .orElseThrow(() -> {
+                                log.info("Product not found: " + stockDecreaseDto.getProductId());
+                                return new ProductNotFoundException();
+                            });
+
+                    product.decreaseStock(stockDecreaseDto.getQuantity());
+                    productRepository.save(product);
+                    productDtoList.add(product.toProductDto());
+                    success = true;
+                } catch (ObjectOptimisticLockingFailureException e){
+                    attempts ++;
+                    if (attempts >= MAX_RETRY_ATTEMPTS) {
+                        log.error("Failed to decrease stock after " + attempts + " attempts for product ID " + stockDecreaseDto.getProductId(), e);
+                        throw new ProductStockUpdateException();
+                    }
+                }
+            }
+        }
+        return productDtoList;
+    }
 }
