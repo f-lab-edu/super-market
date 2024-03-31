8d0b65a7eda0f8b0f7faa335d38f10e80a8be505
hyeoKing <cksgurwkd12@naver.com>
2024-03-31 23:17:57 +0900

    [#27] Product 클래스에 toProductDto 제거 및 클래스 레벨의 @Getter 적용
    [작업 내용]
    - Dto 변환의 책임을 Mapper에 넘기게 되어 `toProductDto` 메서드 제거
    - 클래스 레벨에 @Getter 적용

--- supermarket-api-product/src/main/java/com/harmony/supermarketapiproduct/domain/Product.java
+++ supermarket-api-product/src/main/java/com/harmony/supermarketapiproduct/domain/Product.java
@@ -1,14 +1,16 @@
 package com.harmony.supermarketapiproduct.domain;
 
-import com.harmony.supermarketapiproduct.application.dto.ProductDto;
+
 import com.harmony.supermarketapiproduct.common.exception.ProductInsufficientStockException;
 import com.harmony.supermarketapiproduct.common.exception.ProductQuantityNegativeException;
 import com.harmony.supermarketapiproduct.common.exception.ProductStockMismatchException;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "product")
 public class Product {
@@ -31,8 +33,6 @@ public class Product {
         this.stockQuantity = stockQuantity;
     }
 
-    public ProductDto toProductDto(){ return new ProductDto(this.productId, this.name, this.price, this.stockQuantity); }
-
     public void decreaseStock(final Integer quantity){
         if (quantity < 0) { throw new ProductQuantityNegativeException(); }
         if (this.stockQuantity < quantity){ throw new ProductInsufficientStockException(); }
@@ -43,4 +43,5 @@ public class Product {
     public void verifyStockAmount(final Integer expectedStock){
         if (!this.stockQuantity.equals(expectedStock)) { throw new ProductStockMismatchException(); }
     }
+
 }
