0eb9ad9059976d1c05cd474fd2b89a3e0a9a9941
hyeok <cksgurwkd12@naver.com>
2024-04-02 01:36:47 +0900

    [#31] 프로젝트 실행 시점에 차후 product 테스트를 위한 더미 데이터 삽입
    [작업 내용]
    - 프로젝트 실행시 product를 이용하는 로직 테스트를 위해 테스트용 더미 데이터 삽입
    
    [테스트 내용]
    - h2 콘솔을 이용해 값 정상 삽입 확인

---
+++ supermarket-api-product/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO `product` (`name`, `price`, `stock_quantity`)
+VALUES ('레예스 글러브(빨강)', 300000, 5);
+
+INSERT INTO `product` (`name`, `price`, `stock_quantity`)
+VALUES ('팹시 콜라 제로 210ml(단품)', 300, 100);
+
+INSERT INTO `product` (`name`, `price`, `stock_quantity`)
+VALUES ('m16(구형)', 1000000, 15);
