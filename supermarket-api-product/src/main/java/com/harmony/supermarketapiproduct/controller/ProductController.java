80cc0c27be45c1741823132fd920054f2356df13
hyeok <cksgurwkd12@naver.com>
2024-04-02 00:29:56 +0900

    [#28] 상품 재고 감소를 위한 컨트롤러 개발
    [작업 내용]
    - 프리픽스로 `/api/v1`를 적용해 api set을 유연하게 제공할 수 있도록함
    - 상품 엔티티의 '재고 필드'에 대한 부분적인 수정이기에 Patch Method 적용
    - 요청값에 대한 검증을 위해 @Valid 애노테이션 적용
    - `/products/{productId}/decrease-stock`라는 url을 고려했으나 유효값 검증, DTO 생성의 편의성을 고려해 StockDecreaseDto에서 한번에 처리하는 방법을 채택해 현재 url 채택
    - stockDecreaseDtos는 변경 될 필요가 없어 상수화
    - 현재는 자세한 상태코드나 헤더가 필요치 않다고 판단되 ResponseEntity 미적용
    
    [테스트 내용]
    - Postman API를 이용한 인수 테스트

--- supermarket-api-product/src/main/java/com/harmony/supermarketapiproduct/controller/ProductController.java
+++ supermarket-api-product/src/main/java/com/harmony/supermarketapiproduct/controller/ProductController.java
@@ -1,10 +1,24 @@
 package com.harmony.supermarketapiproduct.controller;
 
+import com.harmony.supermarketapiproduct.application.ProductService;
+import com.harmony.supermarketapiproduct.application.dto.ProductDto;
+import com.harmony.supermarketapiproduct.application.dto.StockDecreaseDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
+
+    @PatchMapping("/products/decrease-stock")
+    public List<ProductDto> decreaseStock(@Valid @RequestBody final List<StockDecreaseDto> stockDecreaseDtos) {
+
+        return productService.decreaseStock(stockDecreaseDtos);
+    }
 
 }
