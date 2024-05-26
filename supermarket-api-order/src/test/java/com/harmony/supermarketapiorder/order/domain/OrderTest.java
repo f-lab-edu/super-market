3044790582ac45a1bf6f2bd33de34d9b92759010
hyeok <cksgurwkd12@naver.com>
2024-05-27 00:38:50 +0900

    [#39] 주문 생성 성공에 대한 통합, 단위 테스트 작성
    - 주문 생성 성공에 대한 통합 테스트 작성
    - 테스트를 쉽게 하기 위해 빌더를 적용해 보다 유연하게 OrderRequest를 생성 가능케함

--- supermarket-api-order/src/test/java/com/harmony/supermarketapiorder/order/domain/OrderTest.java
+++ supermarket-api-order/src/test/java/com/harmony/supermarketapiorder/order/domain/OrderTest.java
@@ -3,9 +3,11 @@ package com.harmony.supermarketapiorder.order.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class OrderTest {
 
@@ -13,14 +15,14 @@ class OrderTest {
     @Test
     void test1() {
         // given
-        OrderRequest request = new OrderRequest(
-                "최찬혁",
-                "풍세로 801-23",
-                "빛보다 빠른 배달",
-                LocalDate.now(),
-                "card",
-                "누구보다 빠르게 와주세요."
-        );
+        OrderRequest request = OrderRequest.builder()
+                .customerId("최찬혁")
+                .deliveryAddress("풍세로 801-23")
+                .deliveryMethod("빛보다 빠른 배달")
+                .expectedDeliveryDate(LocalDate.now())
+                .paymentMethod("card")
+                .specialRequest("누구보다 빠르게 와주세요.")
+                .build();
 
         // when
         Order order = Order.from(request);
@@ -32,5 +34,27 @@ class OrderTest {
         assertEquals("card", order.getPaymentMethod());
         assertEquals("누구보다 빠르게 와주세요.", order.getSpecialRequest());
         assertEquals("pending", order.getStatus());
+    }
+
+
+    @DisplayName("주문에 상품 추가 성공 테스트")
+    @Test
+    void test2() {
+        // given
+        Order order = new Order();
+        OrderItem item = new OrderItem();
+        item.setProductId(1L);
+        item.setPrice(new BigDecimal("29.99"));
+        item.setQuantity(2);
+
+        // when
+        order.addItem(item);
+
+        // then
+        assertNotNull(order.getItems());
+        assertEquals(1, order.getItems().size());
+        assertEquals(item, order.getItems().get(0));
+        assertEquals(order, item.getOrder());
+
     }
 }
