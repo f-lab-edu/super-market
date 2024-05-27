9e3c77bf8452d93e1cb5848eafb2bc5c269c51fc
hyeok <cksgurwkd12@naver.com>
2024-05-27 22:58:59 +0900

    [#39] OrderService의 orderCreate 메서드의 동작을 검증하기 위한 성공, 실패 테스트 작성
    - 성공 테스트에 사용했던 "최찬혁"이라는 customerId는 order 객체 생성 정책 변경으로 인해 사용 불가, 10글자 이하의 정수형으로 변경
    - 주문 생성을 위해 필요한 item은 최소 1개 ~ 10개 까지만 허용하는 정책을 테스트 하기 위해 실패 테스트 작성
    - request가 null이 넘어오는 경우를 막는 정책을 테스트하기 위한 실패 테스트 작성

--- supermarket-api-order/src/main/java/com/harmony/supermarketapiorder/order/application/OrderService.java
+++ supermarket-api-order/src/main/java/com/harmony/supermarketapiorder/order/application/OrderService.java
@@ -17,6 +17,7 @@ public class OrderService {
     // TODO 1. 주문 정보 저장
     @Transactional
     public Order createOrder(OrderRequest request) {
+        validateOrderRequest(request);
         Order order = Order.from(request);
 
         request.getItems().stream()
@@ -34,6 +35,18 @@ public class OrderService {
     // TODO 3. 주문을 취소하는 기능
     public void cancelOrder(){
 
+    }
+
+    private void validateOrderRequest(OrderRequest request) {
+        if (request == null) {
+            throw new IllegalArgumentException("request는 null이 될 수 없습니다.");
+        }
+        if (request.getItems() == null || request.getItems().isEmpty()) {
+            throw new IllegalArgumentException("주문 생성을 위해 최소 1개 이상의 상품 정보가 필요합니다.");
+        }
+        if (request.getItems().size() > Order.MAX_ITEMS) {
+            throw new IllegalStateException("주문을 위한 상품 목록은 " + Order.MAX_ITEMS + "개를 초과할 수 없습니다.");
+        }
     }
 
 }
