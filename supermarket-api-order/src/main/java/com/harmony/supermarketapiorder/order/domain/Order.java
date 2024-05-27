0386fa9d669c71a8c1238b9257a992096da4d9c2
hyeok <cksgurwkd12@naver.com>
2024-05-27 22:53:33 +0900

    [#39] order 도메인 클래스 생성에 대한 검증 로직 확인을 위한 실패 테스트 작성
    - Order 도메인의 from 메서드를 이용해 객체 생성 과정에서 수행되는 valid의 정상 동작을 확인키 위해 실패 테스트 작성

--- supermarket-api-order/src/main/java/com/harmony/supermarketapiorder/order/domain/Order.java
+++ supermarket-api-order/src/main/java/com/harmony/supermarketapiorder/order/domain/Order.java
@@ -2,6 +2,7 @@ package com.harmony.supermarketapiorder.order.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.apache.commons.lang3.StringUtils;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -33,12 +34,16 @@ public class Order {
     @Builder.Default
     private List<OrderItem> items = new ArrayList<>();
 
+    public static final int MAX_ITEMS = 10;
+
     public void addItem(OrderItem item) {
         items.add(item);
         item.setOrder(this);
     }
 
     public static Order from(OrderRequest request) {
+        validateRequest(request);
+
         return Order.builder()
                 .customerId(request.getCustomerId())
                 .orderDate(LocalDateTime.now())
@@ -60,4 +65,20 @@ public class Order {
     public void getOrderDetails() {
 
     }
+
+    private static void validateRequest(OrderRequest request) {
+        if (request == null) {
+            throw new IllegalArgumentException("주문 요청이 빈 상태로 넘어왔습니다.");
+        }
+        if (StringUtils.isBlank(request.getCustomerId()) || !request.getCustomerId().matches("\\d{1,10}")) {
+            throw new IllegalArgumentException("고객 ID는 1~10 사이의 정수형으로 구성되야 합니다.");
+        }
+        if (StringUtils.isBlank(request.getDeliveryAddress()) || StringUtils.isBlank(request.getDeliveryMethod()) || StringUtils.isBlank(request.getPaymentMethod())) {
+            throw new IllegalArgumentException("배송주소, 배송 수단, 지불 수단중에 비어있는 값이 있습니다.");
+        }
+        if (request.getExpectedDeliveryDate() == null || !request.getExpectedDeliveryDate().isAfter(LocalDate.now().plusDays(1)) || !request.getExpectedDeliveryDate().isBefore(LocalDate.now().plusDays(7))) {
+            throw new IllegalArgumentException("예상 배송일은 배송 요청 당일로부터 최소 1일에서 7일 사이로 입력되야 합니다.");
+        }
+    }
+
 }
