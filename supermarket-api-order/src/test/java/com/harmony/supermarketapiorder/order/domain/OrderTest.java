c54d0bf86df5f03021331df5ed6e0aa885b337f3
hyeok <cksgurwkd12@naver.com>
2024-05-27 22:53:33 +0900

    [#39] order 도메인 클래스 생성에 대한 검증 로직 확인을 위한 실패 테스트 작성
    - Order 도메인의 from 메서드를 이용해 객체 생성 과정에서 수행되는 valid의 정상 동작을 확인키 위해 실패 테스트 작성

--- supermarket-api-order/src/test/java/com/harmony/supermarketapiorder/order/domain/OrderTest.java
+++ supermarket-api-order/src/test/java/com/harmony/supermarketapiorder/order/domain/OrderTest.java
@@ -1,60 +1,205 @@
 package com.harmony.supermarketapiorder.order.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class OrderTest {
 
-    @DisplayName("주문 생성 성공 테스트")
-    @Test
-    void test1() {
-        // given
-        OrderRequest request = OrderRequest.builder()
-                .customerId("최찬혁")
-                .deliveryAddress("풍세로 801-23")
-                .deliveryMethod("빛보다 빠른 배달")
-                .expectedDeliveryDate(LocalDate.now())
-                .paymentMethod("card")
-                .specialRequest("누구보다 빠르게 와주세요.")
-                .build();
+    @DisplayName("주문 생성 테스트")
+    @Nested
+    class OrderCreateTest {
+        @DisplayName("주문 생성 성공 테스트")
+        @Test
+        void orderCreateSuccess() {
+            // given
+            OrderRequest request = OrderRequest.builder()
+                    .customerId("123456789")
+                    .deliveryAddress("풍세로 801-23")
+                    .deliveryMethod("빛보다 빠른 배달")
+                    .expectedDeliveryDate(LocalDate.now().plusDays(2))
+                    .paymentMethod("card")
+                    .specialRequest("누구보다 빠르게 와주세요.")
+                    .build();
 
-        // when
-        Order order = Order.from(request);
+            // when
+            Order order = Order.from(request);
 
-        // then
-        assertEquals("최찬혁", order.getCustomerId());
-        assertEquals("풍세로 801-23", order.getDeliveryAddress());
-        assertEquals("빛보다 빠른 배달", order.getDeliveryMethod());
-        assertEquals("card", order.getPaymentMethod());
-        assertEquals("누구보다 빠르게 와주세요.", order.getSpecialRequest());
-        assertEquals("pending", order.getStatus());
+            // then
+            assertEquals("123456789", order.getCustomerId());
+            assertEquals("풍세로 801-23", order.getDeliveryAddress());
+            assertEquals("빛보다 빠른 배달", order.getDeliveryMethod());
+            assertEquals("card", order.getPaymentMethod());
+            assertEquals("누구보다 빠르게 와주세요.", order.getSpecialRequest());
+            assertEquals("pending", order.getStatus());
+        }
+
+        @DisplayName("고객 ID는 최대 10글자 까지 가능하다.")
+        @Test
+        void orderCreateFail1() {
+            // given
+            OrderRequest request = OrderRequest.builder()
+                    .customerId("12345678901")  // 11자리 숫자, 유효하지 않음
+                    .deliveryAddress("풍세로 801-23")
+                    .deliveryMethod("빛보다 빠른 배달")
+                    .expectedDeliveryDate(LocalDate.now().plusDays(3))
+                    .paymentMethod("card")
+                    .specialRequest("누구보다 빠르게 와주세요.")
+                    .build();
+
+            // when & then
+            Exception exception = assertThrows(IllegalArgumentException.class, () -> Order.from(request));
+            assertTrue(exception.getMessage().contains("고객 ID는 1~10 사이의 정수형으로 구성되야 합니다."));
+        }
+        @DisplayName("고객 ID는 최소 한글자 이상이어야 한다.")
+        @Test
+        void orderCreateFail3() {
+            // given
+            OrderRequest request = OrderRequest.builder()
+                    .customerId("")  // 11자리 숫자, 유효하지 않음
+                    .deliveryAddress("풍세로 801-23")
+                    .deliveryMethod("빛보다 빠른 배달")
+                    .expectedDeliveryDate(LocalDate.now().plusDays(3))
+                    .paymentMethod("card")
+                    .specialRequest("누구보다 빠르게 와주세요.")
+                    .build();
+
+            // when & then
+            Exception exception = assertThrows(IllegalArgumentException.class, () -> Order.from(request));
+            assertTrue(exception.getMessage().contains("고객 ID는 1~10 사이의 정수형으로 구성되야 합니다."));
+        }
+
+
+        @DisplayName("예상 배송일은 주문일로부터 7일 안으로만 입력되어야 한다.")
+        @Test
+        void orderCreateFail4() {
+            // given
+            OrderRequest request = OrderRequest.builder()
+                    .customerId("1234567890")
+                    .deliveryAddress("풍세로 801-23")
+                    .deliveryMethod("빛보다 빠른 배달")
+                    .expectedDeliveryDate(LocalDate.now().plusDays(8))  // 8일 경과시 정책 벗어남 유효하지 않음
+                    .paymentMethod("card")
+                    .specialRequest("누구보다 빠르게 와주세요.")
+                    .build();
+
+            // when & then
+            Exception exception = assertThrows(IllegalArgumentException.class, () -> Order.from(request));
+            assertTrue(exception.getMessage().contains("예상 배송일은 배송 요청 당일로부터 최소 1일에서 7일 사이로 입력되야 합니다."));
+        }
+
+        @DisplayName("예상 배송일은 주문일로부터 최소 하루가 지나야 한다.")
+        @Test
+        void orderCreateFail5() {
+            // given
+            OrderRequest request = OrderRequest.builder()
+                    .customerId("1234567890")
+                    .deliveryAddress("풍세로 801-23")
+                    .deliveryMethod("빛보다 빠른 배달")
+                    .expectedDeliveryDate(LocalDate.now())  // 주문 당일은 배송 정책에 부합하지 않음
+                    .paymentMethod("card")
+                    .specialRequest("누구보다 빠르게 와주세요.")
+                    .build();
+
+            // when & then
+            Exception exception = assertThrows(IllegalArgumentException.class, () -> Order.from(request));
+            assertTrue(exception.getMessage().contains("예상 배송일은 배송 요청 당일로부터 최소 1일에서 7일 사이로 입력되야 합니다."));
+        }
+
+        @DisplayName("주문 요청은 null이 들어오면 안된다.")
+        @Test
+        void orderCreateFailRequestNull() {
+            // given
+            OrderRequest request = null;
+
+            // when & then
+            Exception exception = assertThrows(IllegalArgumentException.class, () -> Order.from(request));
+            assertTrue(exception.getMessage().contains("주문 요청이 빈 상태로 넘어왔습니다."));
+        }
+
+        @DisplayName("배송 주소는 비어있으면 안된다.")
+        @Test
+        void orderCreateFailEmptyDeliveryAddress() {
+            // given
+            OrderRequest request = OrderRequest.builder()
+                    .customerId("123456789")
+                    .deliveryAddress(" ")  // 비어있는 배송 주소
+                    .deliveryMethod("빛보다 빠른 배달")
+                    .expectedDeliveryDate(LocalDate.now().plusDays(2))
+                    .paymentMethod("card")
+                    .specialRequest("누구보다 빠르게 와주세요.")
+                    .build();
+
+            // when & then
+            Exception exception = assertThrows(IllegalArgumentException.class, () -> Order.from(request));
+            assertTrue(exception.getMessage().contains("배송주소, 배송 수단, 지불 수단중에 비어있는 값이 있습니다."));
+        }
+
+        @DisplayName("배송 수단이 비어있으면 안된다.")
+        @Test
+        void orderCreateFailNullDeliveryMethod() {
+            // given
+            OrderRequest request = OrderRequest.builder()
+                    .customerId("123456789")
+                    .deliveryAddress("풍세로 801-23")
+                    .deliveryMethod(" ")
+                    .expectedDeliveryDate(LocalDate.now().plusDays(2))
+                    .paymentMethod("card")
+                    .specialRequest("누구보다 빠르게 와주세요.")
+                    .build();
+
+            // when & then
+            Exception exception = assertThrows(IllegalArgumentException.class, () -> Order.from(request));
+            assertTrue(exception.getMessage().contains("배송주소, 배송 수단, 지불 수단중에 비어있는 값이 있습니다."));
+        }
+
+        @DisplayName("지불 수단이 비어 있으면 안된다.")
+        @Test
+        void orderCreateFailEmptyPaymentMethod() {
+            // given
+            OrderRequest request = OrderRequest.builder()
+                    .customerId("123456789")
+                    .deliveryAddress("풍세로 801-23")
+                    .deliveryMethod("빛보다 빠른 배달")
+                    .expectedDeliveryDate(LocalDate.now().plusDays(2))
+                    .paymentMethod(" ")  // 비어있는 지불 수단
+                    .specialRequest("누구보다 빠르게 와주세요.")
+                    .build();
+
+            // when & then
+            Exception exception = assertThrows(IllegalArgumentException.class, () -> Order.from(request));
+            assertTrue(exception.getMessage().contains("배송주소, 배송 수단, 지불 수단중에 비어있는 값이 있습니다."));
+        }
     }
 
+    @DisplayName("주문에 상품 추가 테스트")
+    @Nested
+    class OrderItemAddTest {
+        @DisplayName("주문에 상품 추가 성공 테스트")
+        @Test
+        void orderAddItemSuccess() {
+            // given
+            Order order = new Order();
+            OrderItem item = new OrderItem();
+            item.setProductId(1L);
+            item.setPrice(new BigDecimal("29.99"));
+            item.setQuantity(2);
 
-    @DisplayName("주문에 상품 추가 성공 테스트")
-    @Test
-    void test2() {
-        // given
-        Order order = new Order();
-        OrderItem item = new OrderItem();
-        item.setProductId(1L);
-        item.setPrice(new BigDecimal("29.99"));
-        item.setQuantity(2);
+            // when
+            order.addItem(item);
 
-        // when
-        order.addItem(item);
-
-        // then
-        assertNotNull(order.getItems());
-        assertEquals(1, order.getItems().size());
-        assertEquals(item, order.getItems().get(0));
-        assertEquals(order, item.getOrder());
+            // then
+            assertNotNull(order.getItems());
+            assertEquals(1, order.getItems().size());
+            assertEquals(item, order.getItems().get(0));
+            assertEquals(order, item.getOrder());
+        }
 
     }
+
 }
