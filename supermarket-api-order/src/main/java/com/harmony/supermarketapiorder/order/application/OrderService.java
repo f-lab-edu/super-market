ccd00bb08244e4a9a38b60c03915b1f245b13f09
hyeoKing <cksgurwkd12@naver.com>
2024-06-11 01:32:22 +0900

    [#47] 예외 발생 지점인 서비스 계층에서 예외 처리 후 실패 Dto 반환토록 처리 - 최대한 예외 발생 지점에서 문제를 해결키 위해 예외 대신 서비스 계층에서 로깅 후 실패 Dto 내리도록 개선

--- supermarket-api-order/src/main/java/com/harmony/supermarketapiorder/order/application/OrderService.java
+++ supermarket-api-order/src/main/java/com/harmony/supermarketapiorder/order/application/OrderService.java
@@ -6,11 +6,11 @@ import com.harmony.supermarketapiorder.order.domain.OrderRepository;
 import com.harmony.supermarketapiorder.order.domain.OrderRequest;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class OrderService {
 
@@ -20,13 +20,18 @@ public class OrderService {
 
     // TODO 1. 주문 정보 저장
     @Transactional
-    public OrderDto createOrder(OrderRequest request) {
+    public OrderResponseDto createOrder(OrderRequest request) {
         // TODO 1. 각 요청 실패시 처리 구현 필요
 
         // restTemplate.postForObject(productServiceUrl, request, Void.class); // TODO. 각 서비스 호출을 위한 DTO로 변경
         // restTemplate.postForObject(paymentServiceUrl, request, Void.class); // TODO. 공통 모듈을 통한 응답 처리
 
-        validateOrderRequest(request);
+        String validateResult = validateOrderRequest(request);
+        if(validateOrderRequest(request) != "isOk"){
+            log.error("createOrder request exception, validateOrderRequest: " + validateResult);
+            return new OrderFailDto(validateResult);
+        }
+
         Order newOrder = new Order(request);
 
         request.getItems().stream()
@@ -37,19 +42,26 @@ public class OrderService {
 
         // TODO. 레빗 엠큐를 이용한 배송 등록 요청
 
-        return new OrderDto(createdOrder);
+        return new OrderSuccessDto(createdOrder);
 
     }
 
-    public OrderDto findOrderById(Long orderId){
-        Order order = orderRepository.findById(orderId)
-                .orElseThrow(() -> new IllegalArgumentException("주문을 찾을 수 없습니다."));
+    public OrderResponseDto findOrderById(Long orderId){
+        Order order = null;
 
-        return new OrderDto(order);
+        try {
+            order = orderRepository.findById(orderId).orElseThrow(() -> new IllegalArgumentException("주문을 찾을 수 없습니다."));
+            log.error("findOrderById exception, orderRepository.findById: " + validateResult);
+        } catch (IllegalArgumentException e) {
+            return new OrderFailDto(e.getMessage());
+        }
+
+        return new OrderSuccessDto(order);
     }
 
     @Transactional
     public void cancelOrder(Long orderId){
+        // TODO 1. findOrderById()를 여기서 재사용하기 위해선 findOrderById에서 Dto가 아닌 order 객체를 내려줘야함
         Order order = orderRepository.findById(orderId)
                 .orElseThrow(() -> new IllegalArgumentException("주문을 찾을 수 없습니다."));
 
@@ -58,16 +70,18 @@ public class OrderService {
 
     }
 
-    private void validateOrderRequest(OrderRequest request) {
+    private String validateOrderRequest(OrderRequest request) {
         if (request == null) {
-            throw new IllegalArgumentException("request는 null이 될 수 없습니다.");
+            return "request는 null이 될 수 없습니다.";
         }
         if (request.getItems() == null || request.getItems().isEmpty()) {
-            throw new IllegalArgumentException("주문 생성을 위해 최소 1개 이상의 상품 정보가 필요합니다.");
+            return "주문 생성을 위해 최소 1개 이상의 상품 정보가 필요합니다.";
         }
         if (request.getItems().size() > Order.MAX_ITEMS) {
-            throw new IllegalStateException("주문을 위한 상품 목록은 " + Order.MAX_ITEMS + "개를 초과할 수 없습니다.");
+            return "주문을 위한 상품 목록은 " + Order.MAX_ITEMS + "개를 초과할 수 없습니다.";
         }
+
+        return "isOk";
     }
 
 }
