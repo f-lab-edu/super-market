e60d3e18ab29d621d6f9bbcb341fc709e51c9aba
hyeoKing <cksgurwkd12@naver.com>
2024-06-17 00:28:31 +0900

    [#52] 기본적인 결제 서비스 프로젝트 세팅 - Gradle 설정 파일 세팅 (gradle-wrapper.jar, gradle-wrapper.properties, gradlew, gradlew.bat) - application.properties를 통해 port 번호 8081로 설정 - IntelliJ에서 기본적으로 제공해주는 gitIgnore 적용

---
+++ supermarket-api-payment/src/main/java/com/harmony/supermarketapipayment/SupermarketApiPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.harmony.supermarketapipayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketApiPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermarketApiPaymentApplication.class, args);
+    }
+
+}
